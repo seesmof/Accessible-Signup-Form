21787f2c3dcdacca5025e555f19aa54e49c97bee
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-21 20:58:25 +0300

    style: add horizontal padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Form from "@/components/Form";
 export default function Page() {
   return (
     <div className="bg-linear-to-br to-sky-50 min-h-screen flex items-center justify-center">
-      <div className="container mx-auto bg-white rounded-md outline outline-slate-300 shadow p-5">
-        <Form />
+      <div className="container mx-auto px-4">
+        <div className="bg-white rounded-md outline outline-slate-300 shadow p-5">
+          <Form />
+        </div>
       </div>
     </div>
   );
